3b5ea98b85135bcd678bcb0ea436470219f94efb
Camila Figueiredo <camila_fig@hotmail.com>
2024-05-29 20:32:31 -0300

    shop data

--- src/shop-data.js
+++ src/shop-data.js
@@ -1,11 +1,11 @@
-export const shop_data = [
+export const SHOP_DATA = [
     {
         title: 'Bebidas Alcoólicas',
         items: [
             {
                 id: 1,
                 name: 'Whisky Johnnie Walker Red Label 1000 ml',
-                image: '../src/assets/red-label.png',
+                image: '../src/assets/johnnie-walker.png',
                 price: 116.20,
                 promotionPrice: 89.90,
                 classification: 'Wisky',
@@ -14,7 +14,7 @@ export const shop_data = [
             {
                 id: 2,
                 name: 'Aperitivo Jagermeister 700 ml',
-                image: '../src/assets/',
+                image: '../src/assets/jagermeister.png',
                 price: 148.52,
                 promotionPrice: 112.95,
                 classification: 'Bitter',
@@ -23,7 +23,7 @@ export const shop_data = [
             {
                 id: 3,
                 name: 'Espumante Cava Freixenet Carta Nevada Semi Seco 750 ml',
-                image: '../src/assets/',
+                image: '../src/assets/freixenet.png',
                 price: 85.46,
                 promotionPrice: 69.92,
                 classification: 'Espumante',
@@ -32,7 +32,7 @@ export const shop_data = [
             {
                 id: 4,
                 name: 'Tequila José Cuervo Prata 750 ml',
-                image: '../src/assets/',
+                image: '../src/assets/jose-cuervo.png',
                 price: 109.38,
                 promotionPrice: 99.72,
                 classification: 'Tequila',
@@ -41,7 +41,7 @@ export const shop_data = [
             {
                 id: 5,
                 name: 'Bitter Campari 748 ml',
-                image: '../src/assets/',
+                image: '../src/assets/campari.png',
                 price: 54.30,
                 promotionPrice: 50.06,
                 classification: 'Bitter',
@@ -50,7 +50,7 @@ export const shop_data = [
             {
                 id: 6,
                 name: 'Vodka Cîroc 750 ml',
-                image: '../src/assets/',
+                image: '../src/assets/ciroc.png',
                 price: 217.10,
                 promotionPrice: 154.68,
                 classification: 'Vodca',
@@ -59,7 +59,7 @@ export const shop_data = [
             {
                 id: 7,
                 name: 'Gin Taqueray Dry 750 ml',
-                image: '../src/assets/',
+                image: '../src/assets/taqueray.png',
                 price: 110.99,
                 promotionPrice: 99.00,
                 classification: 'Gin',
@@ -68,7 +68,7 @@ export const shop_data = [
             {
                 id: 8,
                 name: 'Rum Malibu 750 ml',
-                image: '../src/assets/',
+                image: '../src/assets/malibu.png',
                 price: 68.28,
                 promotionPrice: 48.65,
                 classification: 'Run',
@@ -77,7 +77,7 @@ export const shop_data = [
             {
                 id: 9,
                 name: 'Vinho Branco Casal Garcia 750 ml',
-                image: '../src/assets/',
+                image: '../src/assets/garcia.png',
                 price: 69.99,
                 promotionPrice: 65.99,
                 classification: 'Vinho',
@@ -86,7 +86,7 @@ export const shop_data = [
             {
                 id: 10,
                 name: 'Vinho Chilano Sauvignon Blanc 750 ml',
-                image: '../src/assets/',
+                image: '../src/assets/chilano.png',
                 price: 33.00,
                 promotionPrice: 29.00,
                 classification: 'Vinho',
@@ -95,7 +95,7 @@ export const shop_data = [
             {
                 id: 11,
                 name: 'Vinho Concha Y Toro Reservado Cabernet Sauvignon 750 ml',
-                image: '../src/assets/',
+                image: '../src/assets/concha-cabernet.png',
                 price: 33.50,
                 promotionPrice: 26.80,
                 classification: 'Vinho',
@@ -104,7 +104,7 @@ export const shop_data = [
             {
                 id: 12,
                 name: 'Sake Gekkeikan 750 ml',
-                image: '../src/assets/',
+                image: '../src/assets/saque-gek',
                 price: 79.97,
                 promotionPrice: 60.00,
                 classification: 'Saquê',
@@ -113,7 +113,7 @@ export const shop_data = [
             {
                 id: 13,
                 name: 'Cerveja Heineken Long Neck 330 ml',
-                image: '../src/assets/',
+                image: '../src/assets/heineken.png',
                 price: 6.99,
                 promotionPrice: 5.95,
                 classification: 'Cerveja',
@@ -122,7 +122,7 @@ export const shop_data = [
             {
                 id: 14,
                 name: 'Cerveja de Trigo Paulaner Munchen Weissbier 500 ml',
-                image: '../src/assets/',
+                image: '../src/assets/paulaner.png',
                 price: 18.55,
                 promotionPrice: 14.85,
                 classification: 'Cerveja',
@@ -136,7 +136,7 @@ export const shop_data = [
             {
                 id: 29,
                 name: 'Refrigerante Coca-Cola 350 ml',
-                image: '../src/assets/',
+                image: '../src/assets/coca-cola.png',
                 price: 3.80,
                 promotionPrice: 3.30,
                 classification: 'Refrigerante',
@@ -145,7 +145,7 @@ export const shop_data = [
             {
                 id: 30,
                 name: 'Refrigerante Lemon Fresh Sprite 510 ml',
-                image: '../src/assets/',
+                image: '../src/assets/sprite.png',
                 price: 3.59,
                 promotionPrice: 3.29,
                 classification: 'Refrigerante',
@@ -154,7 +154,7 @@ export const shop_data = [
             {
                 id: 31,
                 name: 'Água de Coco Esterilizada Kero Coco 1000 ml',
-                image: '../src/assets/',
+                image: '../src/assets/kero-coco.png',
                 price: 14.95,
                 promotionPrice: 11.21,
                 classification: 'Água de Coco',
@@ -163,7 +163,7 @@ export const shop_data = [
             {
                 id: 32,
                 name: 'Xarope Monin de Cranberry',
-                image: '../src/assets/',
+                image: '../src/assets/monin.png',
                 price: 69.95,
                 promotionPrice: 52.46,
                 classification: 'Xarope',
@@ -172,7 +172,7 @@ export const shop_data = [
             {
                 id: 33,
                 name: 'Água Mineral com Gás S. Pellegrino 250 ml',
-                image: '../src/assets/',
+                image: '../src/assets/agua-gas.png',
                 price: 19.95,
                 promotionPrice: 14.96,
                 classification: 'Água Mineral',
@@ -181,7 +181,7 @@ export const shop_data = [
             {
                 id: 34,
                 name: 'Água Mineral sem Gás Alka+9.1 330 ml',
-                image: '../src/assets/',
+                image: '../src/assets/agua.png',
                 price: 2.69,
                 promotionPrice: 2.47,
                 classification: 'Água Mineral',
@@ -190,7 +190,7 @@ export const shop_data = [
             {
                 id: 35,
                 name: 'Energético Red Bull 250 ml',
-                image: '../src/assets/',
+                image: '../src/assets/red-bull.png',
                 price: 9.10,
                 promotionPrice: 7.90,
                 classification: 'Energético',
@@ -199,7 +199,7 @@ export const shop_data = [
             {
                 id: 36,
                 name: 'Suco Integral de Uva Branca Aurora 1500 ml',
-                image: '../src/assets/',
+                image: '../src/assets/suco-aurora.png',
                 price: 23.95,
                 promotionPrice: 17.95,
                 classification: 'Suco Integral',
@@ -208,7 +208,7 @@ export const shop_data = [
             {
                 id: 37,
                 name: 'Água Tônica Antarctica Zero Açucar 1000 ml',
-                image: '../src/assets/',
+                image: '../src/assets/tonica-zero.png',
                 price: 8.99,
                 promotionPrice: 8.29,
                 classification: 'Água Tônica',
@@ -217,7 +217,7 @@ export const shop_data = [
             {
                 id: 38,
                 name: 'Água Tônica Antarctica 1000 ml',
-                image: '../src/assets/',
+                image: '../src/assets/tonica-normal',
                 price: 8.99,
                 promotionPrice: 8.29,
                 classification: 'Água Tônica',
@@ -226,7 +226,7 @@ export const shop_data = [
             {
                 id: 39,
                 name: 'Xarope Dilute Premium Blue Curaçau e Laranja 500 ml',
-                image: '../src/assets/',
+                image: '../src/assets/dilute.png',
                 price: 32.25,
                 promotionPrice: 27.00,
                 classification: 'Xarope',
@@ -235,7 +235,7 @@ export const shop_data = [
             {
                 id: 40,
                 name: 'Suco Integral de Uva Tinto Garibaldi 1500 ml',
-                image: '../src/assets/',
+                image: '../src/assets/suco-garibaldi.png',
                 price: 24.22,
                 promotionPrice: 20.60,
                 classification: 'Suco',
@@ -244,7 +244,7 @@ export const shop_data = [
             {
                 id: 41,
                 name: 'Energético Monster Energy Zero Açucar 473 ml',
-                image: '../src/assets/',
+                image: '../src/assets/monster.png',
                 price: 10.00,
                 promotionPrice: 9.50,
                 classification: 'Energético',
@@ -253,7 +253,7 @@ export const shop_data = [
             {
                 id: 42,
                 name: 'Água de Coco Integral Villa Piva 1000 ml',
-                image: '../src/assets/',
+                image: '../src/assets/vila-piva.png',
                 price: 23.46,
                 promotionPrice: 18.77,
                 classification: 'Água de Coco',
