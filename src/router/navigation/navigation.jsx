17be89814b924b9e7c03e9d3bbe89961f704c193
Camila Figueiredo <camila_fig@hotmail.com>
2024-05-31 22:09:02 -0300

    correção aula04

--- src/router/navigation/navigation.jsx
+++ src/router/navigation/navigation.jsx
@@ -7,30 +7,27 @@ import './navigation.scss'
 
 export function Navigation() {
     return (
-        <>
-            <div className="navigation">
-                <div className="logo-container">
-                    <div>
-                        <img className='logo' src={logo} alt="Logo da loja" />
-                    </div>
-                    <p className="celebrar-title">Celebrar</p>
-                    <p className="celebrar-subtitle">Casa de Bebidas</p>
+        <div className="navigation">
+            <div className="logo-container">
+                <div>
+                    <img className='logo' src={logo} alt="Logo da loja" />
                 </div>
-                <div className='nav-search'>
-                    <input className='nav-search-text' type="search" placeholder='o que está buscando hoje?' />
-                    <img className='nav-icon-search' src={nav_search} alt="" />
-                </div>
-                <div className="icon-container">
-                    <img className='nav-icon' src={nav_favorite} alt="" />
-                    <div className="nav-link">Favoritos</div>
-                </div>
-                <div className="icon-container">
-                    <img className='nav-icon' src={nav_login} alt="" />
-                    <div className="nav-link">Entrar</div>
-                </div>
-
-                <CartIcon />
+                <p className="celebrar-title">Celebrar</p>
+                <p className="celebrar-subtitle">Casa de Bebidas</p>
             </div>
-        </>
+            <div className='nav-search'>
+                <input className='nav-search-text' type="search" placeholder='o que está buscando hoje?' />
+                <img className='nav-icon-search' src={nav_search} alt="" />
+            </div>
+            <div className="icon-container">
+                <img className='nav-icon' src={nav_favorite} alt="" />
+                <div className="nav-link">Favoritos</div>
+            </div>
+            <div className="icon-container">
+                <img className='nav-icon' src={nav_login} alt="" />
+                <div className="nav-link">Entrar</div>
+            </div>
+            <CartIcon />
+        </div>
     )
 }
