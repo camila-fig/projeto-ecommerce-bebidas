4d5ec45abddd1b54471587e4858038c2b9135bf6
Camila Figueiredo <camila_fig@hotmail.com>
2024-06-25 17:04:50 -0300

    finalizar com login

--- src/router/checkout/checkout.jsx
+++ src/router/checkout/checkout.jsx
@@ -1,37 +1,42 @@
 import './checkout.scss'
 import { useContext } from 'react'
 import { CartContext } from '../../context/cartContext'
-import { CheckoutItem } from '../../components/checkout-item/checkout-item'
 import { Footer } from '../../components/footer/footer'
 import { Button } from '../../components/button/button'
+import { CheckoutPreview } from '../../components/checkout-preview/checkout-preview'
+import { UserContext } from '../../context/userContext'
+import { NavLink } from 'react-router-dom'
 
 export function Checkout() {
 
-    const { cartItems, cartTotal } = useContext(CartContext)
+    const { cartTotal } = useContext(CartContext)
+    const { currentUser } = useContext(UserContext)
 
-    return (
+    if (!currentUser) {
+        return (
         <>
-            <div className='checkout-container'>
-                <div className='checkout-header'>
-                    <span>Produto</span>
-                    <span>Descrição</span>
-                    <span>Quantidade</span>
-                    <span>Subtotal</span>
-                    <span>Remover</span>
-                </div>
-                <div className='checkout-container-total'>
-                    {
-                        cartItems.map((cartItem) => <CheckoutItem cartItem={cartItem} key={cartItem.id} />)
-                    }
-                    <div className='checkout-total'>
-                        <div>
-                            <Button type="button" typeButton="checkout">Ir para forma de pagamento</Button>
-                        </div>
-                        <span className='checkout-total-value'> TOTAL: R$ {cartTotal.toFixed(2).toString().replace(".", ",")}</span >
-                    </div>
-                </div>
-            </div >
+            <CheckoutPreview />
+            <div className='checkout-total'>
+                <NavLink to='/auth'>
+                    <div className='btn-logar'>entre na sua conta para finalizar o pedido ou cadastre-se</div>
+                </NavLink>
+                <span className='checkout-total-value'> TOTAL: R$ {cartTotal.toFixed(2).toString().replace(".", ",")}</span >
+            </div>
             <Footer />
         </>
-    )
+        )
+    } else {
+        return (
+            <>
+                <CheckoutPreview />
+                <div className='checkout-total'>
+                    <div>
+                        <Button type="button" typeButton="checkout">pagamento</Button>
+                    </div>
+                    <span className='checkout-total-value'> TOTAL: R$ {cartTotal.toFixed(2).toString().replace(".", ",")}</span >
+                </div>
+                <Footer />
+            </>
+        )
+    }
 }
