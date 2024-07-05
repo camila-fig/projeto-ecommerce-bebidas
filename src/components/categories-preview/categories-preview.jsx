c3fb1a374eb2c3338b85843f67d2aa0c56a1f932
Camila Figueiredo <camila_fig@hotmail.com>
2024-07-05 13:37:48 -0300

    atualiza banco de dados

--- src/components/categories-preview/categories-preview.jsx
+++ src/components/categories-preview/categories-preview.jsx
@@ -1,9 +1,17 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { CategoryPreview } from "../category-preview/category-preview"
 import { CategoriesContext } from "../../context/categoriesContext"
+import { addCollentionAdDocuments } from "../../utils/firebase"
+import { SHOP_DATA } from "../../shop-data"
 
 export function CategoriesPreview() {
   const { categoriesMap } = useContext(CategoriesContext)
+
+
+  //Salva/atualiza o banco de dados do Firebase conforme meu arquivo shop-data
+// useEffect(() => {
+//   addCollentionAdDocuments('categories', SHOP_DATA)
+// },[])
 
   return (
     <>
