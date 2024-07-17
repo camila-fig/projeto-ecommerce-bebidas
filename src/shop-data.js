a9b9dbb51f2636de451f498046b30960718f68d0
Camila Figueiredo <camila_fig@hotmail.com>
2024-07-17 14:33:04 -0300

    updated

--- src/shop-data.js
+++ src/shop-data.js
@@ -3,7 +3,7 @@ export const SHOP_DATA = [
         title: 'Bebidas Alcoólicas',
         items: [
             {
-                id: 1,
+                id: 0,
                 name: 'Whisky Johnnie Walker Red Label 1000 ml',
                 image: '../src/assets/johnnie-walker.png',
                 price: 116.20,
@@ -13,7 +13,7 @@ export const SHOP_DATA = [
                 stock: 3
             },
             {
-                id: 2,
+                id: 1,
                 name: 'Aperitivo Jagermeister 700 ml',
                 image: '../src/assets/jagermeister.png',
                 price: 148.52,
@@ -23,7 +23,7 @@ export const SHOP_DATA = [
                 stock: 20
             },
             {
-                id: 3,
+                id: 2,
                 name: 'Espumante Cava Freixenet Carta Nevada Semi Seco 750 ml',
                 image: '../src/assets/freixenet.png',
                 price: 85.46,
@@ -32,7 +32,7 @@ export const SHOP_DATA = [
                 stock: 74
             },
             {
-                id: 4,
+                id: 3,
                 name: 'Tequila José Cuervo Prata 750 ml',
                 image: '../src/assets/jose-cuervo.png',
                 price: 109.38,
@@ -41,7 +41,7 @@ export const SHOP_DATA = [
                 stock: 41
             },
             {
-                id: 5,
+                id: 4,
                 name: 'Bitter Campari 748 ml',
                 image: '../src/assets/campari.png',
                 price: 54.30,
@@ -50,7 +50,7 @@ export const SHOP_DATA = [
                 stock: 58
             },
             {
-                id: 6,
+                id: 5,
                 name: 'Vodka Cîroc 750 ml',
                 image: '../src/assets/ciroc.png',
                 price: 217.10,
@@ -59,7 +59,7 @@ export const SHOP_DATA = [
                 stock: 66
             },
             {
-                id: 7,
+                id: 6,
                 name: 'Gin Taqueray Dry 750 ml',
                 image: '../src/assets/taqueray.png',
                 price: 110.99,
@@ -68,7 +68,7 @@ export const SHOP_DATA = [
                 stock: 54
             },
             {
-                id: 8,
+                id: 7,
                 name: 'Rum Malibu 750 ml',
                 image: '../src/assets/malibu.png',
                 price: 68.28,
@@ -77,7 +77,7 @@ export const SHOP_DATA = [
                 stock: 45
             },
             {
-                id: 9,
+                id: 8,
                 name: 'Vinho Branco Casal Garcia 750 ml',
                 image: '../src/assets/garcia.png',
                 price: 69.99,
@@ -86,7 +86,7 @@ export const SHOP_DATA = [
                 stock: 79
             },
             {
-                id: 10,
+                id: 9,
                 name: 'Vinho Chilano Sauvignon Blanc 750 ml',
                 image: '../src/assets/chilano.png',
                 price: 33.00,
@@ -95,7 +95,7 @@ export const SHOP_DATA = [
                 stock: 82
             },
             {
-                id: 11,
+                id: 10,
                 name: 'Vinho Concha Y Toro Reservado Cabernet Sauvignon 750 ml',
                 image: '../src/assets/concha-cabernet.png',
                 price: 33.50,
@@ -104,7 +104,7 @@ export const SHOP_DATA = [
                 stock: 74
             },
             {
-                id: 13,
+                id: 11,
                 name: 'Cerveja Heineken Long Neck 330 ml',
                 image: '../src/assets/heineken.png',
                 price: 6.99,
@@ -113,7 +113,7 @@ export const SHOP_DATA = [
                 stock: 62
             },
             {
-                id: 14,
+                id: 12,
                 name: 'Cerveja de Trigo Paulaner Munchen Weissbier 500 ml',
                 image: '../src/assets/paulaner.png',
                 price: 18.55,
@@ -127,7 +127,7 @@ export const SHOP_DATA = [
         title: 'Bebidas Não Alcoólicas',
         items: [
             {
-                id: 29,
+                id: 13,
                 name: 'Refrigerante Coca-Cola 350 ml',
                 image: '../src/assets/coca-cola.png',
                 price: 3.80,
@@ -136,7 +136,7 @@ export const SHOP_DATA = [
                 stock: 93
             },
             {
-                id: 30,
+                id: 14,
                 name: 'Refrigerante Lemon Fresh Sprite 510 ml',
                 image: '../src/assets/sprite.png',
                 price: 3.59,
@@ -145,7 +145,7 @@ export const SHOP_DATA = [
                 stock: 85
             },
             {
-                id: 31,
+                id: 15,
                 name: 'Água de Coco Esterilizada Kero Coco 1 l',
                 image: '../src/assets/kero-coco.png',
                 price: 14.95,
@@ -154,7 +154,7 @@ export const SHOP_DATA = [
                 stock: 55
             },
             {
-                id: 32,
+                id: 16,
                 name: 'Xarope Monin de Cranberry 700 ml',
                 image: '../src/assets/monin.png',
                 price: 69.95,
@@ -163,7 +163,7 @@ export const SHOP_DATA = [
                 stock: 22
             },
             {
-                id: 33,
+                id: 17,
                 name: 'Água Mineral com Gás S. Pellegrino 250 ml',
                 image: '../src/assets/agua-gas.png',
                 price: 19.95,
@@ -172,7 +172,7 @@ export const SHOP_DATA = [
                 stock: 137
             },
             {
-                id: 34,
+                id: 18,
                 name: 'Água Mineral sem Gás Alka+9.1 330 ml',
                 image: '../src/assets/agua.png',
                 price: 2.69,
@@ -181,7 +181,7 @@ export const SHOP_DATA = [
                 stock: 142
             },
             {
-                id: 35,
+                id: 19,
                 name: 'Energético Red Bull 250 ml',
                 image: '../src/assets/red-bull.png',
                 price: 9.10,
@@ -190,7 +190,7 @@ export const SHOP_DATA = [
                 stock: 125
             },
             {
-                id: 36,
+                id: 20,
                 name: 'Suco Integral de Uva Branca Aurora 1500 ml',
                 image: '../src/assets/suco-aurora.png',
                 price: 23.95,
@@ -199,7 +199,7 @@ export const SHOP_DATA = [
                 stock: 53
             },
             {
-                id: 37,
+                id: 21,
                 name: 'Água Tônica Antarctica Zero Açucar 1000 ml',
                 image: '../src/assets/tonica-zero.png',
                 price: 8.99,
@@ -208,7 +208,7 @@ export const SHOP_DATA = [
                 stock: 77
             },
             {
-                id: 38,
+                id: 22,
                 name: 'Água Tônica Antarctica 1000 ml',
                 image: '../src/assets/tonica-normal',
                 price: 8.99,
@@ -217,7 +217,7 @@ export const SHOP_DATA = [
                 stock: 102
             },
             {
-                id: 39,
+                id: 23,
                 name: 'Xarope Dilute Premium Blue Curaçau e Laranja 500 ml',
                 image: '../src/assets/dilute.png',
                 price: 32.25,
@@ -226,7 +226,7 @@ export const SHOP_DATA = [
                 stock: 12
             },
             {
-                id: 40,
+                id: 24,
                 name: 'Suco Integral de Uva Tinto Garibaldi 1500 ml',
                 image: '../src/assets/suco-garibaldi.png',
                 price: 24.22,
@@ -235,7 +235,7 @@ export const SHOP_DATA = [
                 stock: 52
             },
             {
-                id: 41,
+                id: 25,
                 name: 'Energético Monster Energy Zero Açucar 473 ml',
                 image: '../src/assets/monster.png',
                 price: 10.00,
@@ -244,7 +244,7 @@ export const SHOP_DATA = [
                 stock: 109
             },
             {
-                id: 42,
+                id: 26,
                 name: 'Água de Coco Integral Villa Piva 1000 ml',
                 image: '../src/assets/vila-piva.png',
                 price: 23.46,
